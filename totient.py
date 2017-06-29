2ae86dfc2e9398b08117f3cabb8905da63df346b
zacharytower <zacht7722@gmail.com>
2017-06-28 22:14:24 -0400

    added totient file.

---
+++ totient.py
@@ -0,0 +1,14 @@
+from eratosthenes import coprime
+def totient(n):
+	'''
+	Returns the number of integers k such that, for all
+	1 <= k <= n, k is relatively prime to n.
+	'''
+	total = 0
+	for k in range(1,n + 1):
+		if coprime(n,k):
+			total += 1
+
+	return total
+
+print totient(9)
